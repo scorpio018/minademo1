e8eba743fd7ed7f11efb81f949b4a2343a13f8d7
yangyang <scorpioyoung018@gmail.com>
2013-05-03 17:36:10 +0800

    2013-5-3 17:35
    项目说明文档

---
+++ src/com/scorpio/handler/Num.java
@@ -0,0 +1,13 @@
+package com.scorpio.handler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class Num {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		
+//		String a = URLDecoder.decode("%16%53", "utf-8");
+		char a = 48;
+		System.out.println(a);
+	}
+}
